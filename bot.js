26caa9e5efe8939aec2651d19a0f2e2a150467db
akashuba <kashuba-aleksandr@mail.ru>
2019-11-02 17:05:05 +0300

    Fix password command uppercase

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.on('message', msg => {
     const name = msg.from.first_name;
-    if (msg.text === 'password') {
+    if (msg.text.toLowerCase().indexOf('password') !== -1) {
         bot.sendMessage(msg.chat.id, passwordGenerator.getPassword(8));
     } else {
         bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
